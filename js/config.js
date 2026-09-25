96fa3b0527e0919e0a8102b36c2808bd32cee7e6
Nippon - Wings <nb7gv58s4b@privaterelay.appleid.com>
2026-09-25 21:54:22 +0200

    Fix duplicate declaration of SUPABASE_URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "const SUPABASE_URL = "https://gdzorpepscjkaylmqnal.supabase.co";
+const SUPABASE_URL = "https://gdzorpepscjkaylmqnal.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_thdmKutwucdBA0g6nBCZIg_Swxkk_Jo";
